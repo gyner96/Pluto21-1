b88f3861adf233dc2a5cc3574fbd6528d0b57cb8
admin <gyner.dindari@outlook.de>
2020-10-07 13:45:12 +0200

    Logging onCalled funktionier (siehe Logcat)

--- app/src/main/java/de/hawlandshut/pluto21/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto21/MainActivity.java
@@ -3,12 +3,15 @@ package de.hawlandshut.pluto21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "xx MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);;
+        Log.d(TAG, "onCreate called");
+        setContentView(R.layout.activity_main);
+
     }
 }
